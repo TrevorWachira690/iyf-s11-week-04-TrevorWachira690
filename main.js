4f695c49b248cfe1e9ca571c3ecb028c12154845
George Trevor <greatgaming656@gmail.com>
2026-07-10 05:19:40 +0300

    Finalized: Completed Control flow exercise

--- main.js
+++ main.js
@@ -130,3 +130,107 @@ function reverseString(str) {
     return str.split("").reverse().join("");
 }
 console.log("Reversed:", reverseString("Hello, World!"));
+
+// Function to return tip amount
+function calculateTip(billAmount, tipPercentage= 15) {
+    return (billAmount * tipPercentage) / 100;
+}
+console.log(`Bill amount: $100, Tip percentage: 20%`);
+console.log(`Tip amount: $${calculateTip(100, 20).toFixed(2)}`);
+
+// ================
+// CONTROL FLOW
+// ================
+
+// Exercise 1: If/Else Statements
+function getGrade(score) {
+    if (score >= 90) {
+        return "A";
+    } else if (score >= 80) {
+        return "B";
+    } else if (score >= 70) {
+        return "C";
+    } else if (score >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }
+}
+console.log("Grade for score 85:", getGrade(85));
+console.log("Grade for score 62:", getGrade(72));
+
+// Exercise 2: Switch Statements
+function getDayName(dayNumber) {
+    switch(dayNumber) {
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        default:
+            return "Invalid day number";
+    }
+}
+console.log("Day name for day 1:", getDayName(1));
+console.log("Day name for day 9(Invalid day):", getDayName(9));
+
+// Exercise 3: loops
+// For loop
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// While loop
+let count = 0;
+while (count < 5) {
+    console.log(count);
+    count++;
+}
+
+// For...of (arrays)
+const colors = ["red", "green", "blue"];
+for (const color of colors) {
+    console.log(color);
+}
+
+// Exercises
+// 1. Print numbers from 1 to 100
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+// 2. Print even numbers from 1 to 50
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0)
+        console.log(i);
+    }
+    
+// 3. FizzBuzz: Print numbers from 1 to 100, but for multiples of 3 print "Fizz", for multiples of 5 print "Buzz", and for multiples of both 3 and 5 print "FizzBuzz".
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+// 4. Print a triangle of stars
+for (let i = 1; i <= 5; i++) {
+    let stars = "";
+    for (let j = 1; j <= i; j++) {
+        stars += "*";
+    }
+    console.log(stars);
+}
